5277095cf67c5c2c1f20d6a1a62da27a136e1f88
guillermohache <guillermohache@users.noreply.github.com>
2015-03-27 17:36:14 +0200

    Update testcomputer.c

--- SeaBattle/testcomputer.c
+++ SeaBattle/testcomputer.c
@@ -2,7 +2,7 @@
 #include "human.h"
 #include "coord.h"
 #include <stdio.h>
-int x,a = 0,barcox = 0, barcoy = 0,outfield;
+int x, b = 0, a = 0,barcox = 0, barcoy = 0,outfield;
 int countershoot [2];
 int checkinfield(int); // Check if all coordinates go into the field
 Coord c;
@@ -10,10 +10,18 @@ Coord c;
 main()
 {
     srand(time(NULL));
-    for ( x = 0; x < 300; x++ ) {
+
+for (barcoy = 0; barcoy < 5 ; barcoy++ )
+{
+
+
+for (barcox = 0; barcox < 4 ; barcox++ )
+
+    {
+
+    for ( x = 0; x < 1000; x++ ) {
 
     computerMove();
-
     outfield = checkinfield (c.x);// Check if c.x is inside the field.
 	if (outfield == 1)
 	{
@@ -25,33 +33,47 @@ main()
 	printf("Coodinates out of field!!!!\n");
 	break;}
 
-
     if( c.x == barcox && c.y == (barcoy + a)) //
     {
     tellComputerShipHurted();
     countershoot[a]=x;
+
     a++;
     printf("Este es el numero ordinal del intento %d\n", x);
-    printf(" These are the coordinates\n%d\n", c.x);
-    printf("%d\n \n", c.y);
+
+
+    if(x > 300)
+    {
+    printf("Error. Too many shoots to try to kill one ship\n"); //
+	break;
+    }
+
 
     if (a > 2) //computerlogic has killed the ship
     {
-    if ( (countershoot[2] - countershoot[1]) > 5 || (countershoot[1] - countershoot[0]) > 5 ) //Shoot should not be more than 5.
+    if ( (countershoot[2] - countershoot[1]) > 20 || (countershoot[1] - countershoot[0]) > 20 ) //Shoot should not be more than 5.
     {
 	printf("Error. Too many shoots\n");
+	printf("%d\n", (countershoot[1] - countershoot[0]) );
 	break;
 	}
-
     tellComputerShipKilled();
+    b++;
     a = 0;
 	break;
 	}
     }
-    printf("%d\n", x);
+    else{
+    tellComputerMissed();
     }
+    }
+    }
+    }
+printf("Ships killed %d\n",b);
+}
 
-    }
+
+
 
 
 int checkinfield (int a) {
